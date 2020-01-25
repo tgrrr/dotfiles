c0576afd64d2fb97692f29935d6f3fa4136a9e0b
Phil Steinke <phil@tgrrr.com>
2020-01-25 12:23:57 +0800

    docker aliases enabled

--- aliases.zsh
+++ aliases.zsh
@@ -23,11 +23,11 @@ alias lara="sites && cd laravel/"
 
 # Docker
 alias docker-composer="docker-compose"
-#alias dstop="docker stop $(docker ps -a -q)"
-#alias dpurgecontainers="dstop && docker rm $(docker ps -a -q)"
-#alias dpurgeimages="docker rmi $(docker images -q)"
-#dbuild() { docker build -t=$1 .; }
-#dbash() { docker exec -it $(docker ps -aqf "name=$1") bash; }
+alias dstop="docker stop $(docker ps -a -q)"
+alias dpurgecontainers="dstop && docker rm $(docker ps -a -q)"
+alias dpurgeimages="docker rmi $(docker images -q)"
+dbuild() { docker build -t=$1 .; }
+dbash() { docker exec -it $(docker ps -aqf "name=$1") bash; }
 
 # Git
 alias commit="git add . && git commit -m"
