b0744f93ad83013691a3c4d064dcae93e8ae24f8
Phil Steinke <phil@tgrrr.com>
2020-09-22 20:20:15 +0800

    ansible nextcloud initial commit

---
+++ ansible-playbooks/ansible/install_ansible_ubuntu.sh
@@ -0,0 +1,5 @@
+# run sh install_ansible_ubuntu.sh
+sudo apt update
+sudo apt install software-properties-common
+sudo apt-add-repository --yes --update ppa:ansible/ansible
+sudo apt install ansible
